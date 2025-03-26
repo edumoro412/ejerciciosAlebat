ba655738a9e378de3dddad8c00211042369a3a59
edumoro412 <edumoro2003@gmail.com>
2025-03-26 12:15:59 +0100

    Excercise 1ts done

---
+++ ts/ejercicio1.ts
@@ -0,0 +1,24 @@
+type Bird = {
+  name: string;
+  canEat: boolean;
+  canDrink: boolean;
+  canSleep: boolean;
+  canFly: boolean;
+};
+
+interface Dog extends Bird {
+  race: string;
+  age: number;
+}
+
+const perro: Dog = {
+  name: "Perro",
+  canEat: true,
+  canDrink: true,
+  age: 4,
+  canFly: true,
+  canSleep: true,
+  race: "Golden",
+};
+
+console.log(perro);
