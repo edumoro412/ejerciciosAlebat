3c1bdacc2c634d6dbc987bd2672089ce7d8752de
edumoro412 <edumoro2003@gmail.com>
2025-03-26 11:47:52 +0100

    excercise 5 done

---
+++ ejercicio5.js
@@ -0,0 +1,38 @@
+const myMap = new Map();
+
+function findAnimal() {
+  return true;
+}
+
+const user = {
+  name: "Pedro",
+  last: "Sánchez",
+  age: 45,
+  profesion: "Barro man",
+};
+
+const secondUser = {
+  name: "Pedro",
+  last: "Sánchez",
+  age: 45,
+  salary: 1000,
+  profesion: "Barro man",
+};
+
+myMap.set("moroso", user);
+myMap.set("agarrado", user);
+myMap.set("generoso", secondUser);
+myMap.set("funcion", findAnimal());
+myMap.set("color", "color");
+
+console.log(myMap);
+console.log(myMap.get("moroso"));
+console.log(myMap.has("hola"));
+console.log(myMap.size);
+
+myMap.forEach((valor, clave) => {
+  console.log(`Clave=>${clave}  Valor=>${valor}`);
+});
+
+myMap.clear();
+console.log(myMap);
