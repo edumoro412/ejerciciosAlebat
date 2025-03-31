194b92cd05748f3a95d126af5be2249d41b8da39
edumoro412 <edumoro2003@gmail.com>
2025-03-31 13:46:21 +0200

    sixsteenth excercise done

--- vue/vue-project/src/components/TheCounter.vue
+++ vue/vue-project/src/components/TheCounter.vue
@@ -2,30 +2,37 @@
 import useCounter from '@/composables/counter'
 import TheTitle from './TheTitle.vue'
 import { computed, ref, watch } from 'vue'
+import { useCounterStore } from '@/stores/CounterStore'
 
-const { counter, increment, decrement } = useCounter()
-const multiply = computed(() => counter.value * 2)
+const counterStore = useCounterStore()
 let message = ref('')
 
-watch(counter, (newValue, oldValue) => {
-  if (newValue === 0) {
-    message.value = 'Estás en el valor mínimo'
-  } else if (newValue === 10) {
-    message.value = 'Estás en el valor máximo '
-  } else {
-    message.value = 'Estás en los parámetros adecuados'
-  }
-})
+watch(
+  () => counterStore.count,
+  (newValue) => {
+    if (newValue === 0) {
+      message.value = 'Estás en el valor máximo'
+    } else if (newValue === 10) {
+      message.value = 'Estás en el valor mínimo'
+    } else {
+      message.value = 'Estás en los parámetros adecuados'
+    }
+  },
+)
 </script>
 <template>
   <div class="contador">
     <TheTitle>Titulo estático</TheTitle>
-    <p :class="counter === 10 ? 'contador__green' : 'contador__black'">
-      Contador: {{ counter }} <span> {{ message }}</span>
+    <p :class="counterStore.count === 10 ? 'contador__green' : 'contador__black'">
+      Contador: {{ counterStore.count }}
     </p>
-    <p>El valor de {{ counter }} * 2 = {{ multiply }}</p>
-    <button class="contador__button" v-if="counter < 10" @click="increment">Increment</button>
-    <button class="contador__button" v-if="counter > 0" @click="decrement">Decrement</button>
+    <p>El valor de {{ counterStore.count }} * 2 = {{ counterStore.doubleCount }}</p>
+    <button class="contador__button" v-if="counterStore.count < 10" @click="counterStore.increment">
+      Increment
+    </button>
+    <button class="contador__button" v-if="counterStore.count > 0" @click="counterStore.decrement">
+      Decrement
+    </button>
   </div>
 </template>
 
