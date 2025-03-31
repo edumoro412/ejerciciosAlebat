f91a48e5734dd166b2f3b59b340d4c8f1d7cc6bf
edumoro412 <edumoro2003@gmail.com>
2025-03-31 12:35:00 +0200

    fifteenth excercise done

--- vue/vue-project/src/router.ts
+++ vue/vue-project/src/router.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import CatalogPage from './view/CatalogPage.vue'
 import HomePage from './view/HomePage.vue'
 import ParentPage from './view/ParentPage.vue'
+
 const routes = [
   { path: '/catalog', name: 'catalog', component: CatalogPage },
   { path: '/', name: 'home', component: HomePage },
