77ce570c81a1cfd5d47a409fa667b9881c17c27b
edumoro412 <edumoro2003@gmail.com>
2025-03-31 12:08:28 +0200

    fourteenth excercise done

--- vue/vue-project/src/components/TheCounter.vue
+++ vue/vue-project/src/components/TheCounter.vue
@@ -1,15 +1,28 @@
 <script setup lang="ts">
 import useCounter from '@/composables/counter'
 import TheTitle from './TheTitle.vue'
-import { computed } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 const { counter, increment, decrement } = useCounter()
 const multiply = computed(() => counter.value * 2)
+let message = ref('')
+
+watch(counter, (newValue, oldValue) => {
+  if (newValue === 0) {
+    message.value = 'Estás en el valor mínimo'
+  } else if (newValue === 10) {
+    message.value = 'Estás en el valor máximo '
+  } else {
+    message.value = 'Estás en los parámetros adecuados'
+  }
+})
 </script>
 <template>
   <div class="contador">
     <TheTitle>Titulo estático</TheTitle>
-    <p :class="counter === 10 ? 'contador__green' : 'contador__black'">Contador: {{ counter }}</p>
+    <p :class="counter === 10 ? 'contador__green' : 'contador__black'">
+      Contador: {{ counter }} <span> {{ message }}</span>
+    </p>
     <p>El valor de {{ counter }} * 2 = {{ multiply }}</p>
     <button class="contador__button" v-if="counter < 10" @click="increment">Increment</button>
     <button class="contador__button" v-if="counter > 0" @click="decrement">Decrement</button>
