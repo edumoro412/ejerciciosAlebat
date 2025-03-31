a7a13f1e239ffb4e6464afa855843fbfee4a0c44
edumoro412 <edumoro2003@gmail.com>
2025-03-31 11:57:38 +0200

    thirdteenth excercise done

--- vue/vue-project/src/components/TheCounter.vue
+++ vue/vue-project/src/components/TheCounter.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import useCounter from '@/composables/counter'
 import TheTitle from './TheTitle.vue'
+import { computed } from 'vue'
 
 const { counter, increment, decrement } = useCounter()
+const multiply = computed(() => counter.value * 2)
 </script>
 <template>
   <div class="contador">
     <TheTitle>Titulo estático</TheTitle>
     <p :class="counter === 10 ? 'contador__green' : 'contador__black'">Contador: {{ counter }}</p>
+    <p>El valor de {{ counter }} * 2 = {{ multiply }}</p>
     <button class="contador__button" v-if="counter < 10" @click="increment">Increment</button>
     <button class="contador__button" v-if="counter > 0" @click="decrement">Decrement</button>
   </div>
