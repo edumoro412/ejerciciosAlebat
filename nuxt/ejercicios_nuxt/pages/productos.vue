b0f4cd105814c26683de02b5768143ea5409ab0c
edumoro412 <edumoro2003@gmail.com>
2025-04-01 09:55:16 +0200

    third nuxt excercise done

---
+++ nuxt/ejercicios_nuxt/pages/productos.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import redirectHome from "~/middlewares/redirectHome";
+definePageMeta({
+  middleware: [redirectHome],
+});
+</script>
+
+<template>
+  <div>
+    <h1>Esta es la página de productos</h1>
+  </div>
+</template>
+
+<style scoped></style>
